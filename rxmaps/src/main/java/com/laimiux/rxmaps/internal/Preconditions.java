617a5bb480b59766ddde15b9dea776242eadb219
Laimonas Turauskas <laimonas@codespeck.com>
2015-10-26 10:42:21 -0700

    Fixed problems with bad copy paste of rxmaps/rxmaps-kotlin modules

--- rxmaps/src/main/java/com/laimiux/rxmaps/internal/Preconditions.java
+++ rxmaps/src/main/java/com/laimiux/rxmaps/internal/Preconditions.java
@@ -3,6 +3,10 @@ package com.laimiux.rxmaps.internal;
 import android.os.Looper;
 
 public class Preconditions {
+
+  private Preconditions() {
+  }
+
   public static void checkUiThread() {
     if (Looper.getMainLooper() != Looper.myLooper()) {
       throw new IllegalStateException(
